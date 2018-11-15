a3858eaf5ab7bc846b1096470c71cc1e2a82fe12
chenchen <1517966286@qq.com>
2018-11-15 11:16:21 +0800

    使用模板

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-	return "Hello, World!"
+	return render_template('index.html', title='首页')
