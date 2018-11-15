3e424f208ca5ef23c45aaab4c289fbb3b8bf8b37
chenchen <1517966286@qq.com>
2018-11-15 17:56:43 +0800

    添加User模型

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess me'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' +os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
